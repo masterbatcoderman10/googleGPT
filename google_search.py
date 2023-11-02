fe4c27ac94c0c2d9f609d877ef901585914f1fac
masterbatcoderman10 <suhaib.athar@gmail.com>
2023-11-03 02:49:22 +0400

    added webpage summarizing

--- google_search.py
+++ google_search.py
@@ -37,25 +37,25 @@ def search_results(query):
 # pprint.pprint(search_results('Apple latest product'))
 
 
-def extract_webpage_content(url):
-    try:
-        # Make an HTTP request to the webpage
-        response = requests.get(url)
-        response.raise_for_status()  # Check if the request was successful
-    except requests.HTTPError as http_err:
-        print(f"HTTP error occurred: {http_err}")
-        return None
-    except Exception as err:
-        print(f"An error occurred: {err}")
-        return None
+# def extract_webpage_content(url):
+#     try:
+#         # Make an HTTP request to the webpage
+#         response = requests.get(url)
+#         response.raise_for_status()  # Check if the request was successful
+#     except requests.HTTPError as http_err:
+#         print(f"HTTP error occurred: {http_err}")
+#         return None
+#     except Exception as err:
+#         print(f"An error occurred: {err}")
+#         return None
 
-    # Parse the webpage content with Beautiful Soup
-    soup = BeautifulSoup(response.content, 'html.parser')
+#     # Parse the webpage content with Beautiful Soup
+#     soup = BeautifulSoup(response.content, 'html.parser')
 
-    # Get the text content from the webpage
-    text_content = soup.get_text(separator='\n', strip=True)
+#     # Get the text content from the webpage
+#     text_content = soup.get_text(separator='\n', strip=True)
 
-    return text_content
+#     return text_content
 
 def extract_webpage_content_efficient(url):
 
@@ -73,4 +73,4 @@ page_content =extract_webpage_content_efficient(
 # encoding = tiktoken.encoding_for_model('gpt-3.5-turbo')
 # print(len(encoding.encode(page_content)))
 
-print(len(page_content))
+# print(page_content)
--- gpt_functions.py
+++ gpt_functions.py
@@ -36,9 +36,6 @@ def get_query(input_query):
 
 
 def decide_leads(links, query):
-    """
-    This function uses the openai chat to decide which of the links is the most appropriate for a given query
-    """
 
     sys_message = {
         'role': 'system',
@@ -64,14 +61,62 @@ def decide_leads(links, query):
 # pprint.pprint(get_query('what is apple latest product that is released?'))
 
 
-test_links = {'Apple Accessories for Apple Watch, iPhone, iPad, and Mac': 'https://www.apple.com/shop/accessories/all',
-              'Apple Store Online': 'https://www.apple.com/store',
-              'Every new Apple product coming in 2024 - Macworld': 'https://www.macworld.com/article/671090/new-apple-products.html',
-              'Every product Apple announced this week: iPhone 15 Pro, Apple Watch Series 9, AirPods': 'https://www.zdnet.com/article/every-product-apple-announced-this-week-iphone-15-pro-apple-watch-series-9-airpods/',
-              'The iPhone 15 and Other Apple Products We Still Expect in 2023 - CNET': 'https://www.cnet.com/tech/mobile/iphone-15-and-other-apple-products-we-still-expect-in-2023/',
-              'The new MacBook Pro | Apple - YouTube': 'https://youtube.com/watch?v=0pg_Y41waaE',
-              'Timeline of Apple Inc. products - Wikipedia': 'https://en.wikipedia.org/wiki/Timeline_of_Apple_Inc._products',
-              "What's New - All Accessories - Apple": 'https://www.apple.com/shop/accessories/all/whats-new'}
+def summarize(query, webpage):
 
-pprint.pprint(decide_leads(test_links, 'Apple latest product released'))
+    sys_message = {
+        'role': 'system',
+        'content': 'Your job is to summarize the webpage content based on the query asked by the user. Strictly return the summary as a dictionary with the following key ensuring the summary answers the question concisely and accurately: summary in double quotes. Make sure the answer returned is sufficiently long enough to answer the users question, but at the same time minimal where needed'
+    }
 
+    user_message = {
+        'role': 'user',
+        'content': f"The query is {query}. The webpage is ```{webpage}```."
+    }
+
+    messages = [sys_message, user_message]
+
+    response = openai.ChatCompletion.create(
+        model='gpt-3.5-turbo',
+        temperature=0.5,
+        messages=messages,
+    )
+
+    content = response['choices'][0]['message']['content']
+    content = json.loads(content)
+    return content
+
+
+# test_links = {'Apple Accessories for Apple Watch, iPhone, iPad, and Mac': 'https://www.apple.com/shop/accessories/all',
+#               'Apple Store Online': 'https://www.apple.com/store',
+#               'Every new Apple product coming in 2024 - Macworld': 'https://www.macworld.com/article/671090/new-apple-products.html',
+#               'Every product Apple announced this week: iPhone 15 Pro, Apple Watch Series 9, AirPods': 'https://www.zdnet.com/article/every-product-apple-announced-this-week-iphone-15-pro-apple-watch-series-9-airpods/',
+#               'The iPhone 15 and Other Apple Products We Still Expect in 2023 - CNET': 'https://www.cnet.com/tech/mobile/iphone-15-and-other-apple-products-we-still-expect-in-2023/',
+#               'The new MacBook Pro | Apple - YouTube': 'https://youtube.com/watch?v=0pg_Y41waaE',
+#               'Timeline of Apple Inc. products - Wikipedia': 'https://en.wikipedia.org/wiki/Timeline_of_Apple_Inc._products',
+#               "What's New - All Accessories - Apple": 'https://www.apple.com/shop/accessories/all/whats-new'}
+
+# pprint.pprint(decide_leads(test_links, 'Apple latest product released'))
+
+pprint.pprint(summarize('what is apple latest product that is released?', """The iPhone 15 Pro and iPhone 15 Pro Max were this year's headlining act, with new and improved form factors, updated processors, and the latest advancements in the iPhone camera.
+
+The biggest change, at least on the surface, is the slimmer and lighter design, which has new contoured edges, Grade 5 Titanium, and the thinnest borders ever on an iPhone.
+
+Also: iPhone 15 Pro hands-on: I found a lot of reasons to upgrade and one to wait until next year
+
+The iPhone 15 Pro features the fastest mobile CPU -- A17 Pro. The new CPU can run up to 10% faster than the A16 Bionic chip in last year's iPhone 14 Pro. Compared to Apple's previous iterations, the neural engine is up to two times faster, and the pro-class GPU is up to 20 times faster, optimizing overall smartphone performance.
+
+As for the cameras, the Pro variants feature a 48MP main camera, which allows users to switch between the 24mm, 28mm, and 35mm focal lengths and supports 48MP HEIF images with up to four times the resolution. The Pro Max also features a 5X telephoto lens, the longest optical zoom ever on iPhone.
+
+Like on the iPhone 15 and iPhone 15 Plus, Portrait Mode now runs in the background, so the iPhone can measure the depth information of a photo's subject and later give users the ability to adjust the bokeh effect. The quality of photos in low light will also improve, with a night mode that provides sharper detail and more vivid colors.
+
+Also: 4 key features the iPhone 15 Pro is still missing
+
+Besides the cameras, another major selling point is the Action Button, which replaces the traditional alert slider. The Action Button will function as a ring and silent button by default but can be personalized to be a mappable quick key for turning on the camera app, flashlight, Siri, Shortcuts, and more.
+
+In terms of safety, the phones feature Crash Detection, Emergency SOS via satellite, and a new Roadside Assistant that allows customers to comment to AAA when experiencing car troubles.
+
+Also: Apple iPhone 15 Pro mutes side switch for multifunctional Action button
+
+With the EU imposing a law for electronics makers to adopt USB-C by 2024, the new iPhones finally support charging and data transferring via USB-C, becoming Apple's latest major product to make the switch to the more universal power format.
+
+The phones are available for preorder now in black titanium, white titanium, blue titanium, and natural titanium finishes. The iPhone 15 Pro starts at $999, and the iPhone 15 Pro Max starts at $1,199."""))
