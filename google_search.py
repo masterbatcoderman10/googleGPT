6514288d31b1acd919ca23771837a2a2148ade08
masterbatcoderman10 <suhaib.athar@gmail.com>
2023-11-03 02:27:03 +0400

    added requirements

--- google_search.py
+++ google_search.py
@@ -3,6 +3,8 @@ import http.client
 import dotenv
 import json
 import pprint
+import requests
+from bs4 import BeautifulSoup
 dotenv.load_dotenv()
 
 serp_api_key = os.getenv("SERPER_API_KEY")
@@ -30,3 +32,25 @@ def search_results(query):
     return links
 
 # pprint.pprint(search_results('Apple latest product'))
+
+def extract_webpage_content(url):
+    try:
+        # Make an HTTP request to the webpage
+        response = requests.get(url)
+        response.raise_for_status()  # Check if the request was successful
+    except requests.HTTPError as http_err:
+        print(f"HTTP error occurred: {http_err}")
+        return None
+    except Exception as err:
+        print(f"An error occurred: {err}")
+        return None
+
+    # Parse the webpage content with Beautiful Soup
+    soup = BeautifulSoup(response.content, 'html.parser')
+
+    # Get the text content from the webpage
+    text_content = soup.get_text(separator='\n', strip=True)
+
+    return text_content
+
+pprint.pprint(extract_webpage_content("https://www.zdnet.com/article/every-product-apple-announced-this-week-iphone-15-pro-apple-watch-series-9-airpods/"))
